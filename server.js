42c0d68ef665ddcf2391236be533e920f1e198ed
Manthan <jadhavmanthan33@gmail.com>
2023-07-21 23:19:50 +0530

    second commit

--- server.js
+++ server.js
@@ -10,18 +10,33 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get("/greeting", (req, res) => {
-  return res.send("Hello World");
+  return res.send("Hello---Gunjan");
 });
 
 baseRouter.post("/add", (req, res) => {
-  res.json({ "": null });
+  const { num1, num2 } = req.body;
+  if (isNaN(num1) || isNaN(num2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input. Please provide valid numbers." });
+  }
+  const result = num1 + num2;
+  res.json({ result });
 });
 
 baseRouter.post("/subtract", (req, res) => {
-  res.json({ "": null });
+  const { num1, num2 } = req.body;
+  if (isNaN(num1) || isNaN(num2)) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input. Please provide valid numbers." });
+  }
+  const result = num1 - num2;
+  res.json({ result });
 });
 
 app.use(baseUrl, baseRouter);
+
 app.listen(PORT, () => {
   console.log("Server running at PORT", PORT);
 });
